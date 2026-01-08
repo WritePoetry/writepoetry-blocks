2a41c712cfcff1e5fea8d973f94cbd6cf48831c5
Giacomo Secchi <giacomosecchi@gmail.com>
2026-01-08 21:11:43 +0100

    Scaffold plugin

---
+++ writepoetry-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Writepoetry Blocks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     writepoetry-blocks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Writepoetry_Blocks
+ */
+
+// Your code starts here.
